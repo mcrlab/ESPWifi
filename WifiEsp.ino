41c73d3c81f3208d1788376ae25370ab4eb79a1a
= <=>
2019-02-08 09:17:20 +0000

    Initial Commit

---
+++ WifiEsp.ino
@@ -0,0 +1,103 @@
+#include <WiFiEspClient.h>
+#include <WiFiEsp.h>
+#include <WiFiEspUdp.h>
+#include <PubSubClient.h>
+#include "SoftwareSerial.h"
+
+#define WIFI_AP "PLUSNET-HHQJC9"
+#define WIFI_PASSWORD "f6db6dd64c"
+
+
+// Initialize the Ethernet client object
+WiFiEspClient espClient;
+PubSubClient client(espClient);
+
+char server[] = "192.168.1.3";
+
+SoftwareSerial soft(2, 3); // RX, TX
+
+int status = WL_IDLE_STATUS;
+unsigned long lastSend;
+
+void setup() {
+  // initialize serial for debugging
+  Serial.begin(9600);
+  InitWiFi();
+  client.setServer( server, 1883 );
+  lastSend = 0;
+}
+
+void loop() {
+  status = WiFi.status();
+  if ( status != WL_CONNECTED) {
+    while ( status != WL_CONNECTED) {
+      Serial.print("Attempting to connect to WPA SSID: ");
+      Serial.println(WIFI_AP);
+      // Connect to WPA/WPA2 network
+      status = WiFi.begin(WIFI_AP, WIFI_PASSWORD);
+      delay(500);
+    }
+    Serial.println("Connected to AP");
+  }
+
+  if ( !client.connected() ) {
+    reconnect();
+  }
+
+  if ( millis() - lastSend > 1000 ) { // Update and send only after 1 seconds
+    sendData();
+    lastSend = millis();
+  }
+
+  client.loop();
+}
+
+void InitWiFi()
+{
+  // initialize serial for ESP module
+  soft.begin(9600);
+  // initialize ESP module
+  WiFi.init(&soft);
+  // check for the presence of the shield
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present");
+    // don't continue
+    while (true);
+  }
+
+  Serial.println("Connecting to AP ...");
+  // attempt to connect to WiFi network
+  while ( status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to WPA SSID: ");
+    Serial.println(WIFI_AP);
+    // Connect to WPA/WPA2 network
+    status = WiFi.begin(WIFI_AP, WIFI_PASSWORD);
+    delay(500);
+  }
+  Serial.println("Connected to AP");
+}
+
+void sendData(){
+  String payload = "HelloWorld";
+  char attributes[50];
+  payload.toCharArray( attributes, 50 );
+  client.publish( "hello", attributes );
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Connecting to ThingsBoard node ...");
+    // Attempt to connect (clientId, username, password)
+    if ( client.connect("Arduino Uno Device", NULL, NULL) ) {
+      Serial.println( "[DONE]" );
+    } else {
+      Serial.print( "[FAILED] [ rc = " );
+      Serial.print( client.state() );
+      Serial.println( " : retrying in 5 seconds]" );
+      // Wait 5 seconds before retrying
+      delay( 5000 );
+    }
+  }
+}
+
